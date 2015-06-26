ebac0295f164debda1fcace5b0d604cec563d04c
Andre Borges Medeiros <andborges@gmail.com>
2015-06-26 12:42:58 -0300

    Adicionando formulário

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -5,6 +5,12 @@ angular.module('starter.controllers', [])
 .controller('ChatsCtrl', function($scope, $firebaseObject) {
   var ref = new Firebase("https://sizzling-inferno-5616.firebaseio.com/");
   $scope.chats = $firebaseObject(ref);
+  $scope.message = {};
+
+  $scope.addMessage = function() {
+    ref.push($scope.message);
+    $scope.message.text = "";
+  }
 })
 
 .controller('AccountCtrl', function($scope) {
